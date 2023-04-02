2724dbf474340b9242aa8fa6e0812c6bdc90b138
vivek0042 <vivekjain.0042@gmail.com>
2023-04-02 22:42:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Galleryshow from './Galleryshow'
 
-function App() {
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import("bootstrap");
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <BrowserRouter>
+    <div>
+    {/* <Navbar/> */}
+    <Galleryshow/>
     </div>
-  );
+      
+    // </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/Menu.jsx
@@ -0,0 +1,416 @@
+const Menu=[
+    {
+        id:1,
+        image:"./images1/puri.jpg",
+        name:"Puri",
+        category:"Dinner",
+        watch:"https://www.youtube.com/watch?v=8LR0_Rj2EP8",
+
+
+    },
+    {
+        id:2,
+        image:"./images1/mathri.jpg",
+        name:"Mathri",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=W1VkxNHSqH8",
+        
+
+    },
+    {
+        id:3,
+        image:"images1/khaman.png",
+        name:"Khaman",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=zCN0-bUPWiE",
+        
+
+    },
+    {
+        id:4,
+        image:"../images1/papadchvanu.jpg",
+        name:"Papad Chavanu",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=GLLhNpycpHg",
+        
+
+    },
+    {
+        id:5,
+        image:"./images1/pasta.jpg",
+        name:"Pasta",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=iE5ZkR46_bo",
+        
+
+    },
+    {
+        id:6,
+        image:"./images1/poha.jpg",
+        name:"Poha",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=Dx-BbDtlHQA",
+    
+
+    },
+    {   
+        id:7,
+        image:"./images1/mithololo.png",
+        name:"Mitho Lolo",
+        category:"lunch",
+        watch:"https://www.youtube.com/watch?v=d0zFaPuHqu8",
+        
+
+    },
+    {
+
+        id:8,
+        image:"./images1/bhu.png",
+        name:"Bhugda Bateka",
+        category:"lunch",
+        watch:"https://www.youtube.com/watch?v=TmVjPTb--lA",
+        
+
+    },
+    {
+
+        id:9,
+        image:"./images1/f.png",
+        name:"Manchurian Fried Rice",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=HOX3Fi3Lg54",
+    },
+    {
+        id:10,
+        image:"./images1/am.png",
+        name:"Methi Baingan Sabji",
+        category:"lunch",
+        watch:"https://www.youtube.com/watch?v=BzKOF8zGc8U",
+        
+
+    },
+    {   
+        id:11,
+        image:"./images1/bharvabhindi.png",
+        name:"Bharva Bhindi",
+        category:"lunch",
+        watch:"https://www.youtube.com/watch?v=AGgtXaiugR4",
+        
+
+    },
+    {
+        id:12,
+        image:"./images1/koki.png",
+        name:"Sindhi Koki",
+        category:"breakfat",
+        watch:"https://www.youtube.com/watch?v=ze3_Ibf7gRE",
+        
+
+    },
+    {
+        id:13,
+        image:"./images1/karela.jpg",
+        name:"Karela Paratha",
+        category:"lunch",
+        watch:"https://www.youtube.com/watch?v=KwW3dvizVV4",
+        
+
+    },
+    {
+        
+        id:14,
+        image:"./images1/aloo.png",
+        name:"Bangan Ka Choka",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=Qx3nQyhhblI",
+
+    },
+    {
+        id:15,
+        image:"./images1/corncut.png",
+        name:"Corn Cutlet",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=uY2W22yzZWE",
+        
+
+    },
+    {
+
+        id:16,
+        image:"./images1/e.png",
+        name:"Biscuit Cake",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=2IdGbufmLIY",
+    },
+    {   
+        id:17,
+        image:"./images1/bread.jpg",
+        name:"Bread Pakoda",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=zN3_eUybJvo",
+        
+
+    },
+    {
+        id:18,
+        image:"./images1/sevtamatar.jpg",
+        name:"Sev Tamatar",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=q8w-Klan_4s",
+        
+
+    },
+    {
+        id:19,
+        image:"./images1/s.png",
+        name:"Surati Collegian Bhel",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=8wtmb13RJ4s",
+        
+
+    },
+    {
+        id:20,
+        image:"./images1/buger.jpg",
+        name:"Veg Tawa Burger",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=08KfyDskSmI",
+        
+
+    },
+    {
+
+        id:21,
+        image:"./images1/sindhi.jpg",
+        name:"Sindhi Pakoda",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=KxJ1pRVY6SU",
+    },
+    {
+
+        id:22,
+        image:"./images1/beasn.jpg",
+        name:"Besan ka Chila",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=esDHYPltUVE",
+    },
+    {
+
+        id:23,
+        image:"./images1/ratlami.jpg",
+        name:"Ratlami Sev",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=nDfjSdfrLSE",
+    },
+    {
+
+        id:24,
+        image:"./images1/ice.jpg",
+        name:"I'Kim Sandwich",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=4ZfzLLS0Y1w",
+    },
+    {
+
+        id:25,
+        image:"./images1/kichu.jpg",
+        name:"Khichu",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=utYCaQVatcE",
+    },
+    {
+
+        id:26,
+        image:"./images1/kichu.jpg",
+        name:"Dadi Manu Khichu",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=utYCaQVatcE",
+    },
+    {
+
+        id:27,
+        image:"./images1/chakali.jpg",
+        name:"Sabudana Chakhli",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=DA-JELBwUPU",
+    },
+    {
+
+        id:28,
+        image:"./images1/ra.png",
+        name:"Rava Dhokla",
+        category:"brekfast",
+        watch:"https://www.youtube.com/watch?v=5SFAV7DfQVA",
+    },
+    {
+
+        id:29,
+        image:"./images1/appe.jpg",
+        name:"Appe",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=2XffG23HUXA",
+    },
+    {
+
+        id:30,
+        image:"./images1/gulab.png",
+        name:"Gulab Jamun",
+        category:"lunch",
+        watch:"https://www.youtube.com/watch?v=eeT1pnazXsI",
+    },
+    {
+
+        id:31,
+        image:"./images1/bhel.jpg",
+        name:"Farsan Chat",
+        category:"lunch",
+        watch:"https://www.youtube.com/watch?v=mukrTpEeuU4",
+    },
+    {
+
+        id:32,
+        image:"./images1/gathiya.png",
+        name:"Gathiya Kadhi",
+        category:"lunch",
+        watch:"https://www.youtube.com/watch?v=NU1-YRfLkSY",
+    },
+    {
+
+        id:33,
+        image:"./images1/laddu.png",
+        name:"Coconut Laddu",
+        category:"lunch",
+        watch:"https://www.youtube.com/watch?v=R5e0oA6gT8Y",
+    },
+    {
+
+        id:34,
+        image:"./images1/mpav.png",
+        name:"Masala Pav",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=AS_CIyMoIGE",
+    },
+    {
+
+        id:35,
+        image:"./images1/r.png",
+        name:"Rose Kulfi",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=UD8c-LEHW_I",
+    },
+    {
+
+        id:36,
+        image:"./images1/k.png",
+        name:"Date Tamarind chutney",
+        category:"Dinner",
+        watch:"https://www.youtube.com/watch?v=SjHI7HNwq7Y",
+    },
+    {
+
+        id:37,
+        image:"./images1/khandvi.png",
+        name:"Khandvi",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=0p6cOxrsN4s",
+    },
+    {
+
+        id:38,
+        image:"./images1/juice.png",
+        name:"Watermelon Juice",
+        category:"lunch",
+        watch:"https://www.youtube.com/watch?v=SESDVH89Y4U",
+    },
+    {
+
+        id:39,
+        image:"./images1/bharva.png",
+        name:"Bharva Shimala",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=mg8uyKN0ki4",
+    },
+    {
+
+        id:40,
+        image:"./images1/c.png",
+        name:"Corn Dhokla",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=KFb7GTOB7Pc",
+    },
+    {
+
+        id:41,
+        image:"./images1/jalebi.png",
+        name:"Jalebi",
+        category:"lunch",
+        watch:"https://www.youtube.com/watch?v=NEkFOOkklsM",
+    },
+    {
+
+        id:42,
+        image:"./images1/fafda.png",
+        name:"Fafda",
+        category:"breakfast",
+        watch:"https://www.youtube.com/@AhujasRecipes/search?query=fafda",
+    },
+    {
+
+        id:43,
+        image:"./images1/m.png",
+        name:"Dry Manchurian",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=v-m8p64o4MY",
+    },
+    {
+
+        id:44,
+        image:"./images1/d.png",
+        name:"Dal Moth",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=Uv3Vfaqls9w",
+    },
+    {
+
+        id:45,
+        image:"./images1/a.png",
+        name:"Aloo-Matar Partha",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=iYUnpCvDJMM",
+    },
+    {
+
+        id:46,
+        image:"./images1/i.png",
+        name:"Ice Cream",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=SDYx2WDY4yU",
+    },
+    {
+
+        id:47,
+        image:"./images1/ghug.png",
+        name:"Ghughara",
+        category:"breakfast",
+        watch:"https://www.youtube.com/watch?v=3njdee0dJIk",
+    },
+    {
+
+        id:48,
+        image:"./images1/kela.png",
+        name:"Kela Methi Bhajiya",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=QDVv8FnidR8",
+    },
+    {
+
+        id:49,
+        image:"./images1/f.png",
+        name:"Manchurian Fried Rice",
+        category:"dinner",
+        watch:"https://www.youtube.com/watch?v=HOX3Fi3Lg54",
+    },
+    
+
+];
+export default Menu
